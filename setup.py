ad1dc404d23044b453143f468554436a6c658e27
devpandey2010 <pandeydev2010@gmail.com>
2025-06-29 11:59:25 +0530

    My firrst Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Green_Vision",
+    version="0.0.1",
+    author="Dev Pandey",
+    author_email="devpandey6652@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
